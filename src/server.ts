951c4514100e785536a463e8d99fec47883a6250
AlexDvor <alexmatvichuk@gmail.com>
2026-07-22 09:31:22 +0200

    add example server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
 
-const PORT = process.env.APP_PORT || 3000;
+const PORT = Number(process.env.APP_PORT) || 3000;
 
 console.log('process.env.APP_PORT', process.env.APP_PORT);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+	res.json({ message: 'Hello Home Page!' });
+});
+
+app.get('/app', (req: Request, res: Response) => {
+	res.json({ message: 'Hello App!' });
+});
+
+app.use((req: Request, res: Response) => {
+	res.json({ message: 'Not Found' });
+});
+
 app.listen(PORT, () => {
 	console.log(`Server is running in ${PORT}`);
 });
